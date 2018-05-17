5d6a0d4ab789bc080bb1f8085a6a67757948626f
Joshua M. Clulow <josh@sysmgr.org>
2018-05-17 13:49:05 -0700

    XXX nickname should also not be in colour

--- src/Themes.hs
+++ src/Themes.hs
@@ -337,7 +337,7 @@ usernameAttr i = "username" <> (attrName $ show i)
 
 colorUsername :: Text -> Text -> Widget a
 colorUsername username display =
-  if username == "josh.clulow" then txt (display) else
+  if ((username == "josh.clulow") || (username == "jclulow")) then txt (display) else
   withDefAttr (usernameAttr h) $ txt (display)
     where h = hash username `mod` length usernameColors
 
